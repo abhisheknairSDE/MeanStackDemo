206681ed76e56f24642d9c726fc6ebcee2757047
abhishek-nairr <anair29@asu.edu>
2023-09-07 22:39:40 -0700

    With final demo

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,42 +1,87 @@
-function AppCtrl($scope, $http){
-    console.log("Hello Wold from Controller")
+function AppCtrl($scope, $http) {
+  console.log("Hello Wold from Controller");
 
-   var refresh = function(){
-        $http.get('/contactlist').success(function(response){
-            console.log("I got the data I requested ")
-            console.log("________________"+response)
-            $scope.contactlist = response
-            $scope.contact = "";
-        })
-   }
+  var refresh = function () {
+    $http.get("/contactlist").success(function (response) {
+      $scope.contactlist = response;
+      $scope.contact = "";
+    });
+  };
 
-   refresh();
+  refresh();
 
-    $scope.addContact = function(){
-        console.log($scope.contact)
-        $http.post('/contactlist', $scope.contact).success(function(response){
-            console.log(response)
-            refresh()
-        })
+  $scope.addContact = function () {
+    if (
+      $scope.validateEmail() &&
+      $scope.validateName() &&
+      $scope.validateNumber()
+    ) {
+      console.log($scope.contact);
+      $http.post("/contactlist", $scope.contact).success(function (response) {
+        console.log(response);
+        refresh();
+      });
+    } else {
+      alert("Input Fields are not valid!");
     }
+  };
 
-    $scope.edit = function(id){
-        $http.get('/contactlist/'+id).success(function(response){
-            $scope.contact = response;
-        })
-    }
+  $scope.edit = function (id) {
+    $http.get("/contactlist/" + id).success(function (response) {
+      $scope.contact = response;
+    });
+  };
 
-    $scope.update = function(){
-        $http.put('/contactlist/'+$scope.contact._id, $scope.contact).success(function(response){
-            refresh()
-        })
+  $scope.update = function () {
+    const isNameValid = $scope.validateName();
+    const isEmailValid = $scope.validateEmail();
+    const isNumberValid = $scope.validateNumber();
+    if (isNameValid && isEmailValid && isNumberValid) {
+      $http
+        .put("/contactlist/" + $scope.contact._id, $scope.contact)
+        .success(function (response) {
+          refresh();
+        });
+    } else {
+      alert("Input Fields are not valid!");
     }
+  };
 
-    $scope.remove = function(id){
-        $http.delete('/contactlist/'+id).success(function(response){
-            console.log(response)
-            refresh()
-        })
+  $scope.remove = function (id) {
+    $http.delete("/contactlist/" + id).success(function (response) {
+      console.log(response);
+      refresh();
+    });
+  };
+
+  $scope.validateName = function () {
+    const name = $scope.contact.name;
+    if (/^[A-Z][A-Za-z0-9]*$/.test(name)) {
+      return true;
+    } else {
+      alert(
+        "You have entered an invalid name(name must begin with a capital letter)!"
+      );
+      return false;
     }
-  
+  };
+
+  $scope.validateEmail = function () {
+    const email = $scope.contact.email;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return true;
+    }
+    alert("You have entered an invalid email address!");
+    return false;
+  };
+
+  $scope.validateNumber = function () {
+    const number = $scope.contact.number;
+    if (/^\(\d{3}\) \d{3}-\d{4}$/.test(number)) {
+      return true;
+    } else {
+      alert("Enter a valid number");
+      return false;
+    }
+  };
 }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5500;
@@ -11,8 +11,83 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  writeConcern: {
+    w: "majority",
+  },
+});
 
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connection was successfully estabilished to database");
+});
+
+const peopleSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  number: String,
+});
+
+const People = mongoose.model("People", peopleSchema);
+
+app.get("/contactlist", function (req, res) {
+  People.find({})
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/contactlist", (req, res) => {
+  People.create(req.body)
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/contactlist/:id", function (req, res) {
+  var id = req.params.id;
+  // console.log(id)
+  People.findOne({ _id: id })
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.put("/contactlist/:id", function (req, res) {
+  var id = req.params.id;
+  People.findOneAndUpdate({ _id: id }, req.body, { new: true })
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.error("Error finding and modifying document:", err);
+    });
+});
+
+app.delete("/contactlist/:id", function (req, res) {
+  var id = req.params.id;
+  People.findOneAndDelete({ _id: id })
+    .then((results) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.error("Error finding and deleting document:", err);
+    });
+});
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, () => {
-    console.log('Server is running on port: ' + port);
-})
+  console.log("Server is running on port: " + port);
+});
